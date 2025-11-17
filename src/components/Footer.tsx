7454aff4909aeed817d1396cde8a1ececba357de
Krushnaraj06 <bhosalekrushnaraj@gmail.com>
2025-11-17 18:07:19 +0530

    Changed Node 18 To 20 and Fixed Footer it was causing the error for production deployment.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { Facebook, Instagram, Twitter, Youtube, Linkedin, ArrowRight, Palmtree, Star, MapPin, DollarSign } from 'lucide-react';
 import RimigoWhiteLogo from '../assets/RimigoWhiteLogo.png';
 import ImageCollage from './ImageCollage';
-import Globe from '../assets/Globe4.png'
+import Globe from '../assets/Globe4.png';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -98,7 +98,7 @@ const Footer = () => {
           <div className="flex-shrink-0">
             <img 
               ref={globeRef}
-              src ={Globe}        
+              src={Globe}
               alt="Globe"
               className="w-[600px] h-auto"
             />

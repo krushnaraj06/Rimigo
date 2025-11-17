3560a5ddc3ea30ae3e02b4358ca3184052d8a295
Krushnaraj06 <bhosalekrushnaraj@gmail.com>
2025-11-17 17:45:56 +0530

    Removed globe image cause it was causing error for production deployment on vercel.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,6 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { Facebook, Instagram, Twitter, Youtube, Linkedin, ArrowRight, Palmtree, Star, MapPin, DollarSign } from 'lucide-react';
 import RimigoWhiteLogo from '../assets/RimigoWhiteLogo.png';
 import ImageCollage from './ImageCollage';
-import Globe4 from '../assets/Globe4.png';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -98,7 +97,7 @@ const Footer = () => {
           <div className="flex-shrink-0">
             <img 
               ref={globeRef}
-              src={Globe4}
+              
               alt="Globe"
               className="w-[600px] h-auto"
             />
